12f5b4e560fc1c9c899217b1f458bd1f19578a1e
habaoanh <habaoanh@gmail.com>
2018-06-01 15:12:33 +0800

    phan 1

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Server started."));
